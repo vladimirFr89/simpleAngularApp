a6c356dd003b09a859ba9cad3ba8f14f3575ec69
vladimirfn <silversssnake89@gmail.com>
2018-04-22 13:29:35 +0500

    init a simple angular app add a serve.js for run static server

--- app/js/controllers/simpleCtrl.js
+++ app/js/controllers/simpleCtrl.js
@@ -1,6 +1,15 @@
-angular.module('simpleApp',[]).controller('simpleCtrl', function ($scope) {
+angular.module('simpleApp',[]).controller('simpleCtrl', function ($scope, $timeout) {
     $scope.firstName = "Vladimir";
     $scope.lastName = "Fedorin";
     $scope.salary = 30000;
+    $scope.increaseSalary = function () {
+        if (!$scope.clicked) {
+            $scope.salary = $scope.salary + ($scope.salary * $scope.multiple);
+            $scope.clicked = true;
+            $timeout(function () {
+                $scope.clicked = false;
+            }, 3000);
+        }
+    };
     console.log($scope);
 });
